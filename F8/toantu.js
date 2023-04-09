db03107d2dfff0afa070ac9eab0fe42e57814488
binguyen94 <nhannguyen.tnnnnn@gmail.com>
2023-04-09 21:53:31 +0700

    up

---
+++ F8/toantu.js
@@ -0,0 +1,3 @@
+var number = 6;
+var output = number++ * 2 - --number * 2;
+console.log('output:',output);
---
+++ JavaScript/[Thuchanh]Sudungtoantu/dientichhcn.js
@@ -0,0 +1,14 @@
+// let inputWidth;
+// let inputHeight;
+// inputWidth = prompt('Chiều rộng là');
+// inputHeight = prompt ('Chiều dài là');
+// let width=parseInt(inputWidth);
+// let height=parseInt(inputHeight);
+// let area = width * height;
+// document.write('Diện tích hình chữu nhật là :'+area);
+let width = Number(prompt("Nhập chiều rộng của hình chữ nhật: "));
+let height = Number(prompt("Nhập chiều cao của hình chữ nhật: "));
+
+let area = width * height;
+
+alert('Diện tích của hình chữ nhật là:'+area);
